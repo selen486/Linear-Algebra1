0f392f123513a1cae1a656618145802e8323af86
selen <wmfrpafj14379@gmail.com>
2026-09-08 13:38:01 +0900

    cpp_test

---
+++ cpp_code1.h
@@ -0,0 +1,13 @@
+//
+// Created by wmfrp on 26. 9. 8..
+//
+
+#ifndef LINEAR_ALGEBRA1_CPP_CODE1_H
+#define LINEAR_ALGEBRA1_CPP_CODE1_H
+
+
+class cpp_code1 {
+};
+
+
+#endif //LINEAR_ALGEBRA1_CPP_CODE1_H
